86871624a1259c0ac030d7fa9166e3090d54d3e0
Michael Blaszczyk <michael.i.blaszczyk@gmail.com>
2017-08-04 22:24:38 +0200

    modularize, add webui

---
+++ turing-web/src/main/java/turing/turingweb/TuringServlet.java
@@ -0,0 +1,32 @@
+package turing.turingweb;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinServlet;
+
+import bn.blaszczyk.rosecommon.tools.Preferences;
+import bn.blaszczyk.rosecommon.tools.TypeManager;
+import bn.blaszczyk.roseweb.RoseWebUI;
+
+
+@WebServlet(urlPatterns = "/*", name = "turingservlet", asyncSupported = true)
+@VaadinServletConfiguration(ui = RoseWebUI.class, productionMode = false)
+public class TuringServlet extends VaadinServlet {
+
+	private static final long serialVersionUID = -1132289994117995097L;
+	
+	private static final String ROSE_FILE = "/WEB-INF/classes/turing/resources/tm.rose";
+	
+	@Override
+	public void init(final ServletConfig servletConfig) throws ServletException
+	{
+		super.init(servletConfig);
+		final ServletContext servletContext = servletConfig.getServletContext();
+		TypeManager.parseRoseFile(servletContext.getResourceAsStream(ROSE_FILE));
+		Preferences.setMainClass(TuringServlet.class);
+	}
+    	
+}
